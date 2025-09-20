30b7c1bb34543e84511764d1902c387448f59ce4
AnriAko <ejik.v.tu.m.ane97@gmail.com>
2025-09-20 18:53:27 +0400

    refactor: style change

--- app/components/buttons/sign-out-button/sign-out-button.tsx
+++ app/components/buttons/sign-out-button/sign-out-button.tsx
@@ -19,7 +19,7 @@ export const SignOutButton = () => {
     <button
       onClick={handleLogout}
       data-testid={BUTTON_TEST_IDS.signOut}
-      className="rounded-lg leading-relaxed bg-indigo-500 py-2 text-sm font-medium text-gray-100 hover:bg-indigo-400 transition-colors text-center w-30"
+      className="rounded-lg leading-relaxed cursor-pointer bg-indigo-500 py-2 text-sm font-medium text-gray-100 hover:bg-indigo-400 transition-colors text-center w-30"
     >
       {t('signOut')}
     </button>
